a3ae18161bdbae4300175d5c2d158cfd615d3eec
Masroor Khonkhodzhaev <masroor47@icloud.com>
2024-01-09 19:21:15 -0500

    assignment 7 results

---
+++ assignments/as07/Analytics.sql
@@ -0,0 +1,36 @@
+/* Assignment 5 In-class view of all tables and columns in UDB database */
+CREATE OR REPLACE view v_udb_table_columns as
+select 
+	TABLE_NAME, ORDINAL_POSITION, COLUMN_NAME, DATA_TYPE, coalesce(CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION), IS_NULLABLE
+from 
+	information_schema.columns
+where 
+	table_schema = 'udb'
+ORDER BY TABLE_NAME, ORDINAL_POSITION;
+
+
+/* Select everything from the new view v_udb_table_columns */
+SELECT * FROM v_udb_table_columns;
+
+/* Aggregation: retrieve number of columns per table */
+select table_name, count(column_name) as columns
+from v_udb_table_columns
+group by table_name;
+
+/* Summary of queries */
+SELECT query_assn as assignment, COUNT(*), MIN(query_dur) AS Min_Dur, AVG(query_dur) AS Avg_Dur, MAX(query_dur) AS Max_Dur, SUM(query_dur) AS Total_Dur
+FROM query
+GROUP BY assignment
+ORDER BY assignment;  
+
+/* Retrieve queries for Assignment 3 */
+SELECT * FROM query where query_assn = 'Assignment 3' ORDER BY query_ended LIMIT 100;
+
+/* Retrieve queries for Assignment 4 */
+SELECT * FROM query where query_assn = 'Assignment 4' ORDER BY query_ended LIMIT 100;
+
+/* Retrieve queries for Assignment 5 */ 
+SELECT * FROM query where query_assn = 'Assignment 5' ORDER BY query_ended LIMIT 100;
+
+/* Retrieve queries for Assignment 7 */ 
+SELECT * FROM query where query_assn = 'Assignment 5' ORDER BY query_ended LIMIT 100;
